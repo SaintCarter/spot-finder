ffecfcf0c81110091d51202b675ac8d03704d47f
SaintCarter <cartersaint@protonmail.com>
2026-03-29 14:29:39 -0700

    made map data public

--- server/src/routes/spotData.js
+++ server/src/routes/spotData.js
@@ -7,12 +7,15 @@ import multer from 'multer';
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
+//public
+router.get('/get-map', getMap);
+router.post('/get-spot-media', getSpotMedia);
+router.post('/get-ratings', getRatings);
+
+//private
 router.get('/spot-type', requireAuth, getSpotTypes);
-router.get('/get-map', requireAuth, getMap);
-router.post('/get-spot-media', requireAuth, getSpotMedia);
 router.post('/create-spot', upload.array('spotMedia', 8), requireAuth, createSpot);
 router.post('/rate', requireAuth, insertRating);
-router.post('/get-ratings', requireAuth, getRatings);
 router.post('/update-rating', requireAuth, updateRating);
 router.post('/check-rating', requireAuth, checkUniqueRating);
 
